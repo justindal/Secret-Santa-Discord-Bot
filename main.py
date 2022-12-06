84b3b7ca118443fedc1fdff320468d4a92295039
Justin <justin_daludado10@icloud.com>
2022-12-05 23:45:37 -0500

    added testing functions and main purpose now work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 
-import discord
+import discord, random
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -20,6 +20,7 @@ bot.ssParticipants = dict()
 async def on_ready():
     print(f'successfully logged in as {bot.user}')
 
+
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
@@ -37,11 +38,6 @@ async def printUsers(ctx):
             await ctx.send(member)
 
 @bot.command()
-async def add(ctx, left: int, right: int):
-    """Adds two numbers together."""
-    await ctx.send(left + right)
-
-@bot.command()
 async def join(ctx):
     # dict of message.author to message.author.id
     if ctx.author not in bot.ssParticipants:
@@ -50,12 +46,39 @@ async def join(ctx):
     else:
         await ctx.send(f'{ctx.author} is already in the list!!!!!\n shame on you')
 
-
 @bot.command()
 async def printParticipants(ctx):
     for i in bot.ssParticipants:
         user = bot.get_user(bot.ssParticipants[i])
         await ctx.send(user)
+
+@bot.command()
+async def start(ctx):
+    await ctx.send('starting secret santa with following users:')
+    for i in bot.ssParticipants:
+        user = bot.get_user(bot.ssParticipants[i])
+        await ctx.send(user)
+    await ctx.send('sending dms to users')
+    players = [bot.get_user(bot.ssParticipants[i]) for i in bot.ssParticipants]
+    for i in bot.ssParticipants:
+        randomPlayer = random.choice(players)
+        while randomPlayer == i:
+            randomPlayer = random.choice(players)
+        players.remove(randomPlayer)
+        await i.send(f'your secret santa is {randomPlayer}')
+        await ctx.send(f'sent {randomPlayer} to {i}') # for debugging
+        
+@bot.command()  
+async def testing(ctx):
+    # add all the users to the dict
+    for guild in bot.guilds:
+        for member in guild.members:
+            # add the member to the dict if not bot
+            if member != bot.user:
+                bot.ssParticipants[member] = member.id
+    # remove last user from the dict
+    bot.ssParticipants.popitem() # testing different lengths
+    await ctx.send('added all users to the list')
 
 @bot.command() # test to see of the bot can dm a user
 async def test(ctx):
@@ -63,9 +86,5 @@ async def test(ctx):
         user = bot.get_user(bot.ssParticipants[i])
         await user.send(user)
 
-@bot.command()
-async def start(ctx):
-    await ctx.send('starting secret santa with following users:')
-    printParticipants(ctx)
 
 bot.run(TOKEN)
